62582b69fd76603fd83404cba69ed3b4c722cca6
harshithnutakki2580 <94743818+harshithnutakki2580@users.noreply.github.com>
2024-03-20 18:01:57 +0530

    Add files via upload

---
+++ Variables_Datatype.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variacles_Datatype : MonoBehaviour
+{
+    int myInt = 5;
+    float myFloat = 10.5f;
+    bool myBool = true;
+    public string myString;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("The value of myInt is : " + myInt);
+        Debug.Log("The value of myFloat is : " + myFloat);
+        Debug.Log("The value of myBool is : " + myBool);
+        Debug.Log("The name is : " + myString);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
